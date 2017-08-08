079ee7e06bcc501c08221c0e595b92138e186862
GioLogist <gio@giologist.com>
2017-08-08 04:15:16 -0700

    Thrifter Waves: initial commit

---
+++ src/thrifter/components/waves/waves.js
@@ -0,0 +1,29 @@
+$(function(){
+
+  var l = 0;
+  function thrifterWaveInViewCheck(){
+    var c = $(window).scrollTop();
+    $.each($thrifter_wave__inview, function(){
+      if($(this).isInViewport() && c >= l){
+        $(this).addClass('thrifter-wave-inview--active');
+      } else {
+        $(this).removeClass('thrifter-wave-inview--active');
+      }
+    });
+    l = c;
+  }
+
+  var $thrifter_wave__inview = $('.thrifter-wave-inview');
+  $(window).on('resize scroll', thrifterWaveInViewCheck);
+
+  $.fn.isInViewport = function() {
+      var elementTop = $(this).offset().top;
+      var elementBottom = elementTop + $(this).outerHeight();
+
+      var viewportTop = $(window).scrollTop();
+      var viewportBottom = viewportTop + $(window).height();
+
+      return elementBottom > viewportTop && elementTop < viewportBottom;
+  };
+  
+});
